8c11be785679249d3a36354abf2b6410d66b136f
Karoliina Salo <salo.karoliina@gmail.com>
2022-08-23 18:55:55 +0300

    Beta version ready / 23.8.22 / Karoliina

--- GenshinCharacterBuilder/GenshinCharacterBuilder/Form1.Designer.cs
+++ GenshinCharacterBuilder/GenshinCharacterBuilder/Form1.Designer.cs
@@ -28,13 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.HeaderLB = new System.Windows.Forms.Label();
+            this.VisionLB = new System.Windows.Forms.Label();
+            this.WeaponLB = new System.Windows.Forms.Label();
+            this.RegionLB = new System.Windows.Forms.Label();
+            this.GenderLB = new System.Windows.Forms.Label();
+            this.BodyLB = new System.Windows.Forms.Label();
+            this.VisionRandomLB = new System.Windows.Forms.Label();
+            this.WeaponRandomLB = new System.Windows.Forms.Label();
+            this.RegionRandomLB = new System.Windows.Forms.Label();
+            this.GenderRandomLB = new System.Windows.Forms.Label();
+            this.BodyRandomLB = new System.Windows.Forms.Label();
+            this.RandomizeBT = new System.Windows.Forms.Button();
+            this.CloseLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HeaderLB
+            // 
+            this.HeaderLB.AutoSize = true;
+            this.HeaderLB.BackColor = System.Drawing.Color.Transparent;
+            this.HeaderLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.HeaderLB.Location = new System.Drawing.Point(368, 9);
+            this.HeaderLB.Name = "HeaderLB";
+            this.HeaderLB.Size = new System.Drawing.Size(219, 90);
+            this.HeaderLB.TabIndex = 0;
+            this.HeaderLB.Text = "Genshin Impact\r\nCharacter Builder\r\n";
+            this.HeaderLB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // VisionLB
+            // 
+            this.VisionLB.AutoSize = true;
+            this.VisionLB.BackColor = System.Drawing.Color.Transparent;
+            this.VisionLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VisionLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.VisionLB.Location = new System.Drawing.Point(145, 158);
+            this.VisionLB.Name = "VisionLB";
+            this.VisionLB.Size = new System.Drawing.Size(94, 45);
+            this.VisionLB.TabIndex = 1;
+            this.VisionLB.Text = "Vision";
+            // 
+            // WeaponLB
+            // 
+            this.WeaponLB.AutoSize = true;
+            this.WeaponLB.BackColor = System.Drawing.Color.Transparent;
+            this.WeaponLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WeaponLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.WeaponLB.Location = new System.Drawing.Point(145, 234);
+            this.WeaponLB.Name = "WeaponLB";
+            this.WeaponLB.Size = new System.Drawing.Size(114, 45);
+            this.WeaponLB.TabIndex = 2;
+            this.WeaponLB.Text = "Weapon";
+            // 
+            // RegionLB
+            // 
+            this.RegionLB.AutoSize = true;
+            this.RegionLB.BackColor = System.Drawing.Color.Transparent;
+            this.RegionLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegionLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RegionLB.Location = new System.Drawing.Point(145, 310);
+            this.RegionLB.Name = "RegionLB";
+            this.RegionLB.Size = new System.Drawing.Size(101, 45);
+            this.RegionLB.TabIndex = 3;
+            this.RegionLB.Text = "Region";
+            // 
+            // GenderLB
+            // 
+            this.GenderLB.AutoSize = true;
+            this.GenderLB.BackColor = System.Drawing.Color.Transparent;
+            this.GenderLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenderLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GenderLB.Location = new System.Drawing.Point(508, 158);
+            this.GenderLB.Name = "GenderLB";
+            this.GenderLB.Size = new System.Drawing.Size(104, 45);
+            this.GenderLB.TabIndex = 4;
+            this.GenderLB.Text = "Gender";
+            // 
+            // BodyLB
+            // 
+            this.BodyLB.AutoSize = true;
+            this.BodyLB.BackColor = System.Drawing.Color.Transparent;
+            this.BodyLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BodyLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BodyLB.Location = new System.Drawing.Point(508, 234);
+            this.BodyLB.Name = "BodyLB";
+            this.BodyLB.Size = new System.Drawing.Size(135, 45);
+            this.BodyLB.TabIndex = 5;
+            this.BodyLB.Text = "Body type";
+            // 
+            // VisionRandomLB
+            // 
+            this.VisionRandomLB.AutoSize = true;
+            this.VisionRandomLB.BackColor = System.Drawing.Color.Transparent;
+            this.VisionRandomLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VisionRandomLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.VisionRandomLB.Location = new System.Drawing.Point(299, 158);
+            this.VisionRandomLB.Name = "VisionRandomLB";
+            this.VisionRandomLB.Size = new System.Drawing.Size(88, 45);
+            this.VisionRandomLB.TabIndex = 6;
+            this.VisionRandomLB.Text = "label1";
+            this.VisionRandomLB.Visible = false;
+            // 
+            // WeaponRandomLB
+            // 
+            this.WeaponRandomLB.AutoSize = true;
+            this.WeaponRandomLB.BackColor = System.Drawing.Color.Transparent;
+            this.WeaponRandomLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WeaponRandomLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.WeaponRandomLB.Location = new System.Drawing.Point(299, 234);
+            this.WeaponRandomLB.Name = "WeaponRandomLB";
+            this.WeaponRandomLB.Size = new System.Drawing.Size(88, 45);
+            this.WeaponRandomLB.TabIndex = 7;
+            this.WeaponRandomLB.Text = "label1";
+            this.WeaponRandomLB.Visible = false;
+            // 
+            // RegionRandomLB
+            // 
+            this.RegionRandomLB.AutoSize = true;
+            this.RegionRandomLB.BackColor = System.Drawing.Color.Transparent;
+            this.RegionRandomLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegionRandomLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RegionRandomLB.Location = new System.Drawing.Point(299, 310);
+            this.RegionRandomLB.Name = "RegionRandomLB";
+            this.RegionRandomLB.Size = new System.Drawing.Size(88, 45);
+            this.RegionRandomLB.TabIndex = 8;
+            this.RegionRandomLB.Text = "label1";
+            this.RegionRandomLB.Visible = false;
+            // 
+            // GenderRandomLB
+            // 
+            this.GenderRandomLB.AutoSize = true;
+            this.GenderRandomLB.BackColor = System.Drawing.Color.Transparent;
+            this.GenderRandomLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenderRandomLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GenderRandomLB.Location = new System.Drawing.Point(674, 158);
+            this.GenderRandomLB.Name = "GenderRandomLB";
+            this.GenderRandomLB.Size = new System.Drawing.Size(88, 45);
+            this.GenderRandomLB.TabIndex = 9;
+            this.GenderRandomLB.Text = "label1";
+            this.GenderRandomLB.Visible = false;
+            // 
+            // BodyRandomLB
+            // 
+            this.BodyRandomLB.AutoSize = true;
+            this.BodyRandomLB.BackColor = System.Drawing.Color.Transparent;
+            this.BodyRandomLB.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BodyRandomLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BodyRandomLB.Location = new System.Drawing.Point(674, 234);
+            this.BodyRandomLB.Name = "BodyRandomLB";
+            this.BodyRandomLB.Size = new System.Drawing.Size(88, 45);
+            this.BodyRandomLB.TabIndex = 10;
+            this.BodyRandomLB.Text = "label1";
+            this.BodyRandomLB.Visible = false;
+            // 
+            // RandomizeBT
+            // 
+            this.RandomizeBT.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomizeBT.Location = new System.Drawing.Point(808, 431);
+            this.RandomizeBT.Name = "RandomizeBT";
+            this.RandomizeBT.Size = new System.Drawing.Size(131, 47);
+            this.RandomizeBT.TabIndex = 11;
+            this.RandomizeBT.Text = "Randomize!";
+            this.RandomizeBT.UseVisualStyleBackColor = true;
+            this.RandomizeBT.Click += new System.EventHandler(this.RandomizeBT_Click);
+            // 
+            // CloseLB
+            // 
+            this.CloseLB.AutoSize = true;
+            this.CloseLB.BackColor = System.Drawing.Color.Transparent;
+            this.CloseLB.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseLB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CloseLB.Location = new System.Drawing.Point(914, 9);
+            this.CloseLB.Name = "CloseLB";
+            this.CloseLB.Size = new System.Drawing.Size(33, 38);
+            this.CloseLB.TabIndex = 12;
+            this.CloseLB.Text = "X";
+            this.CloseLB.Click += new System.EventHandler(this.CloseLB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::GenshinCharacterBuilder.Properties.Resources.FW9T4SJaAAAGz1v;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(951, 490);
+            this.Controls.Add(this.CloseLB);
+            this.Controls.Add(this.RandomizeBT);
+            this.Controls.Add(this.BodyRandomLB);
+            this.Controls.Add(this.GenderRandomLB);
+            this.Controls.Add(this.RegionRandomLB);
+            this.Controls.Add(this.WeaponRandomLB);
+            this.Controls.Add(this.VisionRandomLB);
+            this.Controls.Add(this.BodyLB);
+            this.Controls.Add(this.GenderLB);
+            this.Controls.Add(this.RegionLB);
+            this.Controls.Add(this.WeaponLB);
+            this.Controls.Add(this.VisionLB);
+            this.Controls.Add(this.HeaderLB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label HeaderLB;
+        private System.Windows.Forms.Label VisionLB;
+        private System.Windows.Forms.Label WeaponLB;
+        private System.Windows.Forms.Label RegionLB;
+        private System.Windows.Forms.Label GenderLB;
+        private System.Windows.Forms.Label BodyLB;
+        private System.Windows.Forms.Label VisionRandomLB;
+        private System.Windows.Forms.Label WeaponRandomLB;
+        private System.Windows.Forms.Label RegionRandomLB;
+        private System.Windows.Forms.Label GenderRandomLB;
+        private System.Windows.Forms.Label BodyRandomLB;
+        private System.Windows.Forms.Button RandomizeBT;
+        private System.Windows.Forms.Label CloseLB;
     }
 }
 
--- GenshinCharacterBuilder/GenshinCharacterBuilder/Form1.cs
+++ GenshinCharacterBuilder/GenshinCharacterBuilder/Form1.cs
@@ -16,5 +16,43 @@ namespace GenshinCharacterBuilder
         {
             InitializeComponent();
         }
+
+        private void RandomizeBT_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(); // new random to randomize strings
+            string[] Visions = { "Cryo", "Hydro", "Anemo", "Pyro", "Dendro", "Geo", "Electro" }; // strings to randomize
+            string[] Weapons = { "Sword", "Polearm", "Bow", "Claymore", "Catalyst" };
+            string[] Regions = { "Mondstadt", "Liyue", "Inazuma", "Sumeru" };
+            string[] Genders = { "Female", "Male" };
+            string[] Bodytypes = { "Short", "Medium", "Tall" };
+
+            int Vision = rnd.Next(Visions.Length); // Randomize the wanted string
+            int Weapon = rnd.Next(Weapons.Length);
+            int Region = rnd.Next(Regions.Length);
+            int Gender = rnd.Next(Genders.Length);
+            int BodyType = rnd.Next(Bodytypes.Length);
+
+            VisionRandomLB.Text = Visions[Vision]; // displays the randomized text
+            WeaponRandomLB.Text = Weapons[Weapon];
+            RegionRandomLB.Text = Regions[Region];
+            GenderRandomLB.Text = Genders[Gender];
+            BodyRandomLB.Text = Bodytypes[BodyType];
+
+            VisionRandomLB.Visible = true; // brings the hidden label back to visible
+            WeaponRandomLB.Visible = true;
+            RegionRandomLB.Visible = true;
+            GenderRandomLB.Visible = true;
+            BodyRandomLB.Visible = true;
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CloseLB_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
