eb7b33d71a434d2f857fbb77ef5c5939503017b8
Karoliina Salo <salo.karoliina@gmail.com>
2022-09-26 09:26:22 +0300

    Json visuaalista testaamista / 26.9.22 / K.

---
+++ JsonVisTestausta/JsonVisTestausta/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace JsonVisTestausta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TiedotBtn = new System.Windows.Forms.Button();
+            this.TiedotLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TiedotBtn
+            // 
+            this.TiedotBtn.Location = new System.Drawing.Point(662, 391);
+            this.TiedotBtn.Name = "TiedotBtn";
+            this.TiedotBtn.Size = new System.Drawing.Size(126, 47);
+            this.TiedotBtn.TabIndex = 0;
+            this.TiedotBtn.Text = "Näytä tiedot";
+            this.TiedotBtn.UseVisualStyleBackColor = true;
+            this.TiedotBtn.Click += new System.EventHandler(this.TiedotBtn_Click);
+            // 
+            // TiedotLB
+            // 
+            this.TiedotLB.AutoSize = true;
+            this.TiedotLB.Location = new System.Drawing.Point(57, 37);
+            this.TiedotLB.Name = "TiedotLB";
+            this.TiedotLB.Size = new System.Drawing.Size(61, 13);
+            this.TiedotLB.TabIndex = 1;
+            this.TiedotLB.Text = "Universities";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TiedotLB);
+            this.Controls.Add(this.TiedotBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TiedotBtn;
+        private System.Windows.Forms.Label TiedotLB;
+    }
+}
+
---
+++ JsonVisTestausta/JsonVisTestausta/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonVisTestausta
+{
+    public class Student
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Universities
+    {
+        public string university
+        {
+            get;
+            set;
+        }
+
+        public IList<Student> students
+        {
+            get;
+            set;
+        }
+    }
+    public class ClassUniversities
+    {
+        public Universities universities
+        {
+            get;
+            set;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TiedotBtn_Click(object sender, EventArgs e)
+        {
+           ClassUniversities university1 = new ClassUniversities();
+           
+            university1.universities = new Universities();
+
+            university1.universities.university = "South Carolina State University";
+
+            List<Student> listStudent = new List<Student>();
+            Student student1 = new Student
+            {
+                name = "Stephen Cousins"
+            };
+            Student student2 = new Student
+            {
+                name = "Austin A. Newton"
+            };
+            Student student3 = new Student
+            {
+                name = "Adam Wilhite"
+            };
+            Student student4 = new Student
+            {
+                name = "Enis Kurtay YILMAZ"
+            };
+
+            listStudent.Add(student1);
+            listStudent.Add(student2);
+            listStudent.Add(student3);
+            listStudent.Add(student4);
+
+            university1.universities.students = listStudent;
+
+            string json = JsonConvert.SerializeObject(university1);
+
+            TiedotLB.Text = json;
+        
+        }
+    }
+}
